caf6626a9b1cd6ebf4c327efcfd79e2b211d4cf6
Ignacio Castaneda97 <castanedaignacio25@gmail.com>
2025-02-18 21:59:45 -0500

    añadiendo cambios

--- script.js
+++ script.js
@@ -7,3 +7,10 @@
     window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
   });
 
+  document.getElementById("contactoBtn").addEventListener("click", function() {
+    const email = 'castanedaignacio25@gmail.com';
+    const subject = 'Asunto del mensaje';
+    const body = 'Cuerpo del mensaje';
+    
+    window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+  });
