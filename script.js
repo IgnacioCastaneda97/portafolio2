024c4e1543cb7ad4c36d9d29d0c71f1960734c09
Ignacio Castaneda97 <castanedaignacio25@gmail.com>
2025-02-18 22:21:39 -0500

    icono en pestaña del navegador

--- script.js
+++ script.js
@@ -14,3 +14,10 @@
     
     window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
   });
+  document.getElementById("contactoBtn1").addEventListener("click", function() {
+    const email = 'castanedaignacio25@gmail.com';
+    const subject = 'Asunto del mensaje';
+    const body = 'Cuerpo del mensaje';
+    
+    window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+  });
